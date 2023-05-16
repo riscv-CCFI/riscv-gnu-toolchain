9e850468e6064e74be419227e695065fae692631
gppchs <giuseppe.chessa@mail.polimi.it>
2023-05-16 21:43:16 +0200

    corrected script.sh file, now it also makes the project

--- script.sh
+++ script.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 git submodule update --init --recursive
 cp riscv-binutils.patch binutils/
+cd binutils
 patch -p 1 < riscv-binutils.patch
+cd ..
 cp riscv-gcc.patch gcc/
+cd gcc
 patch -p 1 < riscv-gcc.patch
+cd ..
+mkdir ToolInstallationZone
+export ABSOLUTEPATH=$(pwd)
+./configure --prefix=$ABSOLUTEPATH/ToolInstallationZone
+make
